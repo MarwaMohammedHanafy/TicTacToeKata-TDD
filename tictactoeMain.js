f34cdfc129348d0c3cdb870a8ea19447cfd3b39f
Marwa <marwama3bade@gmail.com>
2021-11-02 17:10:05 +0000

    Test & implement if a game is over when all fields in a column are taken by a player

--- tictactoeMain.js
+++ tictactoeMain.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {*} board 
+ * @returns 
+ */
 const rowIsTaken = board => {
     if (!board) throw new "The board is required";
     for (let row = 0; row < 3; row++) {
@@ -8,10 +13,12 @@ const rowIsTaken = board => {
             return 'Player O Is A WINNER';
         }
     }
-
-
 };
-
+/**
+ * 
+ * @param {*} board 
+ * @returns 
+ */
 const diagonalIsTaken = board => {
     if (!board) throw new "The board is required";
     if ((board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O')
@@ -20,10 +27,26 @@ const diagonalIsTaken = board => {
     if ((board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') || (board[2][0] === 'X' && board[1][1] === 'X' && board[0][2] === 'X'))
         return 'Player X Is A WINNER';
 };
-
+/**
+ * 
+ * @param {*} board 
+ * @returns 
+ */
+const columnIsTaken = board => {
+    if (!board) throw new "The board is required";
+    for (let col = 0; col < 3; col++) {
+        if (board[0][col] === 'X' && board[1][col] === 'X' && board[2][col] === 'X') {
+            return 'Player X Is A WINNER';
+        }
+        if (board[0][col] === 'O' && board[1][col] === 'O' && board[2][col] === 'O') {
+            return 'Player O Is A WINNER';
+        }
+    }
+};
 
 
 module.exports = {
     rowIsTaken,
-    diagonalIsTaken
+    diagonalIsTaken,
+    columnIsTaken
 };
--- tictactoeMain.test.js
+++ tictactoeMain.test.js
@@ -1,6 +1,7 @@
 const {
     rowIsTaken,
-    diagonalIsTaken
+    diagonalIsTaken,
+    columnIsTaken
 } = require("./tictactoeMain.js");
 
 //a game is over when all fields in a row are taken by a player
@@ -66,6 +67,40 @@ describe("Test a game is over when all fields in a diagonal are taken by a playe
         ];
         //Act 
         const result = diagonalIsTaken(board);
+        //Assert
+        expect(result).toBe('Player O Is A WINNER');
+    });
+    
+});
+
+//a game is over when all fields in a column are taken by a player
+describe("Test a game is over when all fields in a column are taken by a player ", () => {;
+    test('it throws an error if not send the board ', () => {
+        expect(() => {
+            columnIsTaken();
+        }).toThrow("The board is required");
+    });
+    test('return X ,If all three cells in any column are X', () => {
+        //Arrange 
+        const board = [
+            ["X", "X", null],
+            ["X", null, "0"],
+            ["X", null, "0"]
+        ];
+        //Act 
+        const result = columnIsTaken(board);
+        //Assert
+        expect(result).toBe('Player X Is A WINNER');
+    });
+    test('return O ,If all three cells in any column are O', () => {
+        //Arrange 
+        const board = [
+            ["O", "X", "O"],
+            ["X", null, "O"],
+            ["O", null, "O"]
+        ];
+        //Act 
+        const result = columnIsTaken(board);
         //Assert
         expect(result).toBe('Player O Is A WINNER');
     });
